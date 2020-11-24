67c53c1361a8837042d98e1de5f8df70524e995e
Mauricio Hidalgo <darthsqlus@gmail.com>
2020-11-23 23:30:50 -0800

    NET5 Demo
    completed demo with .NET 5

---
+++ NET5_Sample/Data/MvcMovieContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NET5_Sample.Models;
+
+namespace NET5_Sample.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
